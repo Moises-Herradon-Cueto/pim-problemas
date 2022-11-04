c21e7e9e98a822f6bf45a6dfae074c1b6f7c4b92
MoisesPotato <28111500+MoisesPotato@users.noreply.github.com>
2022-11-04 01:35:04 +0100

    Refactoring

---
+++ parse_lib/src/merge.rs
@@ -0,0 +1,142 @@
+use crate::data::Data;
+
+use crate::preamble::into_template;
+use crate::process_tex::find_year;
+use regex::Regex;
+
+use ParseResult::*;
+
+pub enum ParseResult {
+    Template,
+    ToChange(String),
+}
+
+/// .
+///
+/// # Panics
+///
+/// Panics if I mess up.
+#[must_use]
+pub fn merge_string_data(input: &str, data: &mut Data) -> ParseResult {
+    if is_template(input, data) {
+        return Template;
+    }
+
+    let problem_regex = Regex::new(r"(?s)\\begin\{ejer\}(.*)\\end\{ejer\}").expect("regex wrong");
+    let problem = problem_regex
+        .captures_iter(input)
+        .next()
+        .unwrap()
+        .get(1)
+        .unwrap()
+        .as_str();
+
+    let sol_regex = [
+        Regex::new(r"(?s)\\begin\{proof\}\[Solución\](.*)\\end\{proof\}").expect("regex wrong"),
+        Regex::new(r"(?s)\{\\bf Soluci\\'on:\}(.*)\\end\{document\}").expect("regex wrong"),
+        Regex::new(r"(?s)\{\\bf Solución:\}(.*)\\end\{document\}").expect("regex wrong"),
+    ];
+
+    let solution = sol_regex
+        .iter()
+        .flat_map(|regex| regex.captures_iter(input))
+        .next()
+        .ok_or(format!("{data:#?}"))
+        .expect("Didn't find solution")
+        .get(1)
+        .unwrap()
+        .as_str();
+
+    let paquetes_1: String = Regex::new(r"\\usepackage\[(.*)\]\{(.*)}")
+        .expect("messed up")
+        .captures_iter(input)
+        .filter_map(|result| {
+            let option = result.get(1).unwrap().as_str();
+            let package = result.get(2).unwrap().as_str();
+            if [
+                "inputenc", "babel", "pim", "graphicx", "amssymb", "latexsym", "amsmath", "amsthm",
+                "verbatim",
+            ]
+            .contains(&package)
+            {
+                return None;
+            }
+            Some(format!("\\usepackage[{option}]{{{package}}}\n"))
+        })
+        .collect();
+
+    let paquetes_2: String = Regex::new(r"\\usepackage\{(.*)}")
+        .expect("messed up")
+        .captures_iter(input)
+        .flat_map(|result| {
+            let packages = result.get(1).unwrap().as_str().split(',');
+            packages
+                .filter(|package| {
+                    ![
+                        "inputenc", "babel", "pim", "graphicx", "amssymb", "latexsym", "amsmath",
+                        "amsthm", "verbatim",
+                    ]
+                    .contains(package)
+                })
+                .map(|package| format!("\\usepackage{{{package}}}\n"))
+        })
+        .collect();
+
+    let tikz_libraries: String = Regex::new(r"\\usetikzlibrary\{(.*)}")
+        .expect("messed up")
+        .captures_iter(input)
+        .map(|result| {
+            let package = result.get(1).unwrap().as_str();
+            format!("\\usetikzlibrary{{{package}}}\n")
+        })
+        .collect();
+    let pgfplotsets: String = Regex::new(r"\\pgfplotsset\{(.*)}")
+        .expect("messed up")
+        .captures_iter(input)
+        .map(|result| {
+            let package = result.get(1).unwrap().as_str();
+            format!("\\pgfplotsset{{{package}}}\n")
+        })
+        .collect();
+    let mut temas = data.temas.join(", ");
+
+    if temas.is_empty() {
+        temas = "%".into();
+    }
+
+    let id = data.id;
+
+    let mut fuente = &data.fuente;
+    let percent = String::from("%");
+
+    if fuente.is_empty() {
+        fuente = &percent;
+    }
+    let mut comentarios = data.comentarios.join(", ");
+
+    if comentarios.is_empty() {
+        comentarios = "%".into();
+    }
+
+    ToChange(into_template(
+        &paquetes_1,
+        &paquetes_2,
+        &tikz_libraries,
+        &pgfplotsets,
+        &temas,
+        &fuente,
+        &&comentarios,
+        &&id,
+        &problem,
+        &solution,
+    ))
+}
+
+fn is_template(input: &str, data: &mut Data) -> bool {
+    if input.contains("%%% PLANTILLA PARA SUBIR EJERCICIOS A LA BASE DE DATOS DEL PIM") {
+        find_year(input, data);
+        true
+    } else {
+        false
+    }
+}
--- parse_lib/src/preamble.rs
+++ parse_lib/src/preamble.rs
@@ -1,1 +1,127 @@
+use std::fmt::Display;
 
+pub fn into_template<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10>(
+    paquetes_1: &T1,
+    paquetes_2: &T2,
+    tikz_libraries: &T3,
+    pgfplotsets: &T4,
+    temas: &T5,
+    fuente: &T6,
+    comentarios: &T7,
+    id: &T8,
+    problem: &T9,
+    solution: &T10,
+) -> String
+where
+    T1: Display,
+    T2: Display,
+    T3: Display,
+    T4: Display,
+    T5: Display,
+    T6: Display,
+    T7: Display,
+    T8: Display,
+    T9: Display,
+    T10: Display,
+{
+    format!(
+        "
+% !TeX encoding = UTF-8
+
+
+
+%%% PLANTILLA PARA SUBIR EJERCICIOS A LA BASE DE DATOS DEL PIM
+\\documentclass[12pt,a4paper]{{article}}
+\\usepackage[utf8]{{inputenc}}
+\\usepackage[spanish]{{babel}}
+\\usepackage{{pim}}
+
+% Si necesitas más paquetes, añádelos debajo de la siguiente línea
+%%% Paquetes extra
+{paquetes_1}
+{paquetes_2}
+{tikz_libraries}
+{pgfplotsets}
+%%% Fin de paquetes extra
+
+
+% Introduce los temas separados por comas
+% Por ejemplo
+% \\temas{{
+% Inducción, Numeritos
+% }}
+\\temas{{
+{temas}
+}}
+
+% Dificultad del 1 al 10
+% \\dificultad{{
+% 10
+% }}
+\\dificultad{{
+%
+}}
+
+% De dónde viene el problema
+% \\fuente{{
+% Aritmética de Diofanto, capítulo 1.
+% }}
+\\fuente{{
+{fuente}
+}}
+
+% Curso a partir del cual se puede poner el problema
+% Opciones:
+% 1Primaria, 2Primaria ... 6Primaria
+% 1ESO, 2ESO, 3ESO, 4ESO
+% 1Bach, 2Bach
+% \\curso{{
+% 1ESO
+% }}
+\\curso{{
+%
+}}
+
+% Descomentar para restringir el acceso:
+%\\acceso{{
+%Sí
+%}}
+
+% Comentarios, separados por comas
+% \\comentarios{{
+% Un problema muy fácil, les salió a todos
+% }}
+\\comentarios{{
+{comentarios}
+}}
+
+\\id{{
+{id}
+}}
+
+\\begin{{document}}
+
+\\datos
+
+
+
+ 
+\\begin{{ejer}}
+{problem}
+\\end{{ejer}}
+
+
+ 
+
+ 
+ 
+\\begin{{proof}}[Solución]
+{solution}
+\\end{{proof}}
+
+\\end{{document}}
+
+    
+    "
+    )
+}
